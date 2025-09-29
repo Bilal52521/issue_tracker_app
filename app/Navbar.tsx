d28be4a3de9495eff726bd7d774b7d6b687b3afe
Hazrat Bilal <hazratbilal52521@gmail.com>
2025-09-29 23:17:38 +0500

    Style the Active Link in the Navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,23 +1,35 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { BsFillBugFill } from "react-icons/bs";
+import classNames from "classnames";
 
 const Navbar = () => {
+  const currentPath = usePathname();
+
+  console.log(currentPath);
+
   const links = [
     { lable: "Dashboard", href: "/" },
     { lable: "Issues", href: "/issues" },
   ];
 
   return (
-    <nav className="flex gap-x-9 border-1 h-15 items-center mb-5 px-10">
+    <nav className="flex gap-x-9 shadow-2xs h-15 items-center mb-5 px-10">
       <Link href="/" className="flex gap-3 items-center">
         <BsFillBugFill /> IssueTracker
       </Link>
       <ul className="flex gap-x-4">
         {links.map((lnk) => (
           <Link
-            className="text-zinc-500 hover:text-zinc-800"
-            key="lnk"
+            className={classNames({
+              "text-zinc-900": lnk.href === currentPath,
+              "text-zinc-500": lnk.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            key={lnk.href}
             href={lnk.href}
           >
             {lnk.lable}
---
+++ app/issues/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Issues_Page = () => {
+  return <div>Issues Page</div>;
+};
+
+export default Issues_Page;
